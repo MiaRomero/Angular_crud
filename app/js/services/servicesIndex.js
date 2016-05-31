d14975c8c7f2849dd4c938a257663e6bfa34ae7e
MARIA ROMERO <mariaromero18e@gmail.com>
2016-05-31 10:31:55 -0700

    initial commit communication branch

--- app/js/services/servicesIndex.js
+++ app/js/services/servicesIndex.js
@@ -1,5 +1,6 @@
 module.exports = exports = function(app) {
   require('./animalResource')(app);
   require('./ohMyResource')(app);
+  require('./communicationService')(app);
   require('./appErrorHandler')(app);
 };
